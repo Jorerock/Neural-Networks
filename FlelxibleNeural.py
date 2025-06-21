01a3b137da1a33910e340c81192175f2b97503c9
Jorerock <jorerock@laposte.net>
2025-06-21 03:22:41 +0200

    Refactor FlexibleNeuralNetwork class to Neuronne; add weight and bias initialization, support for softmax activation, and update layers and NeuralNetwork classes accordingly.

--- FlelxibleNeural.py
+++ FlelxibleNeural.py
@@ -3,10 +3,13 @@ import matplotlib.pyplot as plt
 from ImportImage import import_image
 
 
-class FlexibleNeuralNetwork:
+class Neuronne:
     def __init__(self, activation , number_0f_neurons):
         self.activation = activation
         self.number_0f_neurons = number_0f_neurons
+        self.weights = None
+        self.bias = None
+        print("Activation:", self.activation, "Nombre de neurones:", self.number_0f_neurons)
         
     def initialize_weights(self, input_size):
         self.weights = np.random.randn(input_size, 1)
@@ -21,13 +24,16 @@ class FlexibleNeuralNetwork:
             return np.maximum(0, x)
         elif self.activation == 'tanh':
             return np.tanh(x)
+        elif self.activation == 'softmax':
+            exp_x = np.exp(x - np.max(x))
+            return exp_x / exp_x.sum(axis=0, keepdims=True)
         else:
             raise ValueError("Activation non supportée")
     
     def forward(self, inputs):
         if self.weights is None or self.bias is None:
-            self.initialize_weights(len(inputs))
-        # Calcul de la somme pondérée
+            if self.weights is None:
+                self.initialize_weights(len(inputs))
         Sommepondere = np.dot(inputs, self.weights) + self.bias
 
         output = self.choose_activation(Sommepondere)
@@ -36,26 +42,38 @@ class FlexibleNeuralNetwork:
         
 
 
-# # Classe pour créer des couches de neurones flexibles
-# class layers:
-#     def __init__(self, activation, number_0f_neurons):
-#         self.activation = activation
-#         self.number_0f_neurons = number_0f_neurons
-#         self.network = FlexibleNeuralNetwork(activation, number_0f_neurons)
+# Classe pour créer des couches de neurones flexibles
+class layers:
+    def __init__(self, activation, number_0f_neurons):
+        self.activation = activation
+        self.number_0f_neurons = number_0f_neurons
+        self.network = Neuronne(activation, number_0f_neurons)
   
-#     def Result(self, inputs):
-#         for i in range(self.number_0f_neurons):
-#             # Réinitialiser les poids et le biais pour chaque itération
-#             self.network.initialize_weights(len(inputs))
-            
-#             # Calculer la sortie
-#             sortie = self.network.forward(inputs)
-#             # print("layer 1, neuronne", i, ":", sortie)
-#             layer[i] = self.network.forward(inputs).item()
+    def Result(self, inputs):
+        result = [None] * self.number_0f_neurons
+        for i in range(self.number_0f_neurons):
+            self.network.initialize_weights(len(inputs))
+            result[i] = self.network.forward(inputs).item()
+        return result
 
-#         return layer
+
+# Classe pour créer un réseau de neurones flexible
+class NeuralNetwork:
+    def __init__(self, numberofLayer,Layer_neurons_and_activation = [('sigmoid', 128), ('relu', 64), ('relu', 10)]):
+        self.numberofLayer = numberofLayer
+        self.Layer_neurons_and_activation = Layer_neurons_and_activation
+        self.layers = [None] * numberofLayer
+        for i in range(numberofLayer):
+            activation, number_0f_neurons = Layer_neurons_and_activation[i]
+            self.layers[i] = layers(activation, number_0f_neurons)
+
     
-    
+    def Go(self, inputs):
+        for i in range(self.numberofLayer):
+            inputs = self.layers[i].Result(inputs)
+        #une fonction softmax pour la dernière couche de sortie
+        exp_x = np.exp(inputs - np.max(inputs))
+        return exp_x / exp_x.sum(axis=0, keepdims=True)
 
 
 
@@ -68,68 +86,93 @@ inputs = import_image.import_image_npy("Numbers/number.npy")
 inputs = inputs.flatten()  # Aplatir l'image en un vecteur
 inputs = inputs / 255.0  # Normaliser les valeurs des pixels entre 0 et 1
 
-# Afficher les entrées
-print("Entrées normalisées:", inputs)
-network1 = FlexibleNeuralNetwork(activation='sigmoid',number_0f_neurons=128)
 
-# Premier réseau de neurones
-layer = [None] * network1.number_0f_neurons
-for i in range(network1.number_0f_neurons):
-    # Réinitialiser les poids et le biais pour chaque itération
-    network1.initialize_weights(len(inputs))
+
+NeuralNetwork1 = NeuralNetwork(numberofLayer=3, Layer_neurons_and_activation=[('sigmoid', 128), ('relu', 64), ('relu', 10)])
+
+for i in range(3):
+    resultat = (NeuralNetwork1.Go(inputs))
+    print(resultat)
+    for i in range(resultat.shape[0]):
+        print(i+1, ":", resultat[i]/100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+#trash place:
+
+# # Afficher les entrées
+# print("Entrées normalisées:", inputs)
+# layer1 = layers(activation='sigmoid', number_0f_neurons=128)
+# inputs = layer1.Result(inputs)  
+
+# # Premier réseau de neurones
+# # network1 = Neuronne(activation='sigmoid',number_0f_neurons=128)
+# # layer = [None] * network1.number_0f_neurons
+# # for i in range(network1.number_0f_neurons):
+# #     # Réinitialiser les poids et le biais pour chaque itération
+# #     network1.initialize_weights(len(inputs))
     
-    # Calculer la sortie
-    sortie = network1.forward(inputs)
-    # print("layer 1, neuronne", i, ":", sortie)
-    layer[i] = network1.forward(inputs).item()
+# #     # Calculer la sortie
+# #     sortie = network1.forward(inputs)
+# #     # print("layer 1, neuronne", i, ":", sortie)
+# #     layer[i] = network1.forward(inputs).item()
 
-# print(layer)
-
-# deuxième réseau de neurones
-network2 = FlexibleNeuralNetwork(activation='relu',number_0f_neurons=64)
-inputs = layer
-layer2 = [None] * network2.number_0f_neurons
-for i in range(network2.number_0f_neurons):
-    # Réinitialiser les poids et le biais pour chaque itération
-    network2.initialize_weights(len(inputs))
+# # print(layer)
+# # deuxième réseau de neurones
+# network2 = Neuronne(activation='relu',number_0f_neurons=64)
+# # inputs = layerResult
+# layer2 = [None] * network2.number_0f_neurons
+# for i in range(network2.number_0f_neurons):
+#     # Réinitialiser les poids et le biais pour chaque itération
+#     network2.initialize_weights(len(inputs))
     
-    # Calculer la sortie
-    sortie = network2.forward(inputs)
-    # print("layer 2, neuronne", i, ":", sortie)
-    layer2[i] = network2.forward(inputs).item()
+#     # Calculer la sortie
+#     sortie = network2.forward(inputs)
+#     # print("layer 2, neuronne", i, ":", sortie)
+#     layer2[i] = network2.forward(inputs).item()
 
-# print(layer2)
+# # print(layer2)
 
-# Sortie
-network3 = FlexibleNeuralNetwork(activation='relu',number_0f_neurons=10)
-inputs = layer2
-layer3 = [None] * network3.number_0f_neurons
-for i in range(network3.number_0f_neurons):
-    # Réinitialiser les poids et le biais pour chaque itération
-    network3.initialize_weights(len(inputs))
+# # Sortie
+# network3 = Neuronne(activation='relu',number_0f_neurons=10)
+# inputs = layer2
+# layer3 = [None] * network3.number_0f_neurons
+# for i in range(network3.number_0f_neurons):
+#     # Réinitialiser les poids et le biais pour chaque itération
+#     network3.initialize_weights(len(inputs))
     
-    # Calculer la sortie
-    sortie = network3.forward(inputs)
-    # print("layer 3, neuronne", i, ":", sortie)
-    layer3[i] = network3.forward(inputs).item()
-# print(layer3)
+#     # Calculer la sortie
+#     sortie = network3.forward(inputs)
+#     # print("layer 3, neuronne", i, ":", sortie)
+#     layer3[i] = network3.forward(inputs).item()
+# # print(layer3)
 
 
-# Affichage des sorties finales
-print("Sorties finales des couches:")
-print("Layer 1:", layer)
-print("Layer 2:", layer2)
-print("Layer 3:", layer3)
+# # Affichage des sorties finales
+# print("Sorties finales des couches:")
+
+# print("Layer 2:", layer2)
+# print("Layer 3:", layer3)
 
 
-# # Visualisation des sorties des couches
-# plt.figure(figsize=(10, 6))     
+# # # Visualisation des sorties des couches
+# # plt.figure(figsize=(10, 6))     
 
-# for i in range(4):
-#     plt.plot(layer[i], label=f'Layer {i+1} ({network1.activation})')
-# plt.title('Sorties des couches avec activation ' + network1.activation)
-# plt.xlabel('Index de la couche')
-# plt.ylabel('Valeur de sortie')
-# plt.legend()
-# plt.grid(True)
-# plt.show()
+# # for i in range(4):
+# #     plt.plot(layer[i], label=f'Layer {i+1} ({network1.activation})')
+# # plt.title('Sorties des couches avec activation ' + network1.activation)
+# # plt.xlabel('Index de la couche')
+# # plt.ylabel('Valeur de sortie')
+# # plt.legend()
+# # plt.grid(True)
+# # plt.show()
